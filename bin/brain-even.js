c76479ad02c9c19206870efd4fd8b1ddd7f84692
Romanov55 <Romanov20let@gmail.com>
2022-10-14 22:31:12 +0600

    add brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -8,42 +8,36 @@ const name = readlineSync.question('May I have your name? ');
 
 console.log(`Hello, ${name}!`);
 
-console.log ("Answer 'yes' if number even otherwise answer 'no'");
+console.log("Answer 'yes' if number even otherwise answer 'no'");
 
-let i = 0
+let i = 0;
 do {
-    const rand = Math.floor(Math.random() * (100))
+  const rand = Math.floor(Math.random() * (100));
 
-    console.log('Question: ' + rand);
+  console.log(`Question: ${rand}`);
 
-    const answer = readlineSync.question('Your answer: ');
+  const answer = readlineSync.question('Your answer: ');
 
-    let result;
-    if (rand % 2 === 0) {
+  let result;
+  if (rand % 2 === 0) {
     result = 'yes';
-    }
-    else {
+  } else {
     result = 'no';
-    }
+  }
 
-    if (result === answer && i < 2) {
-        console.log('Correct!');
-    }
-    else if (i === 2 && result === answer) {
-        console.log('Correct!');
-        console.log('Congratulations, ' + name + '!');
-    }
-    else {
-        if (answer === 'yes') {
-            console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
-            console.log("Let's try again, " + name + '!');
-            i = i + 3;
-        }
-        else {
-            console.log("'no' is wrong answer ;(. Correct answer was 'yes'.");
-            console.log("Let's try again, " + name + '!');
-            i = i + 3;
-        }
-    }
-    i = i + 1;
+  if (result === answer && i < 2) {
+    console.log('Correct!');
+  } else if (i === 2 && result === answer) {
+    console.log('Correct!');
+    console.log(`Congratulations, ${name}!`);
+  } else if (answer === 'yes') {
+    console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
+    console.log(`Let's try again, ${name}!`);
+    i += 3;
+  } else {
+    console.log("'no' is wrong answer ;(. Correct answer was 'yes'.");
+    console.log(`Let's try again, ${name}!`);
+    i += 3;
+  }
+  i += 1;
 } while (i < 3);
