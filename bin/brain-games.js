8b935d18fb9c750fa66312439c100750c55b2f23
Romanov55 <Romanov20let@gmail.com>
2022-10-14 11:28:47 +0600

    add brain-games

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
-import name from '../index.js'
+import readlineSync from 'readline-sync'
 
 console.log('Welcome to the Brain Games!')
 
-name 
+const name = readlineSync.question('May I have your name? ');
 
 console.log('Hello, '+name+'!')
