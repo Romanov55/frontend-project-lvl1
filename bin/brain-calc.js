8e20a23092785788f8a1aad2e23d7c0191c797df
Romanov55 <Romanov20let@gmail.com>
2022-10-17 10:30:21 +0600

    add brain-calc

---
+++ bin/brain-calc.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!');
+
+const name = readlineSync.question('May I have your name? ');
+
+console.log(`Hello, ${name}!`);
+
+console.log("What is the result of the expression?");
+
+let i = 0;
+do {
+  const rand = Math.floor(Math.random() * (100));
+  const rand2 = Math.floor(Math.random() * (100));
+
+  const randomOperator = Math.floor(Math.random() * (3 - 0) + 0);
+
+  let example;
+
+  if (randomOperator === 0) {
+    console.log(`Question: ${rand + rand2}`);
+    example = rand + rand2;
+  }
+  else if (randonOperator === 1) {
+    console.log(`Question: ${rand - rand2}`);
+    example = rand - rand2;
+  }
+  else {
+    console.log(`Question: ${rand * rand2}`);
+    example = rand * rand2;
+  }
+
+  const answer = readlineSync.question('Your answer: ');
+
+  if (example === answer && i < 2) {
+    console.log('Correct!');
+  } else if (i === 2 && example === answer) {
+    console.log('Correct!');
+    console.log(`Congratulations, ${name}!`);
+  } else {
+    console.log( `'${answer}' is wrong answer ;(. Correct answer was '${example}'.`);
+    console.log(`Let's try again, ${name}!`);
+    i += 3;
+  }
+  i += 1;
+} while (i < 3);
