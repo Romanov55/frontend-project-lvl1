f6fc1e4052beb278396507bfe19a542be82d61fa
Romanov55 <Romanov20let@gmail.com>
2022-10-18 21:07:27 +0600

    add brain-progression

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,30 +1,38 @@
 #!/usr/bin/env node
 
 import readlineSync from 'readline-sync';
-import {welcome, name} from '../src/index.js';
+import { name } from '../src/index.js';
 
-welcome;
+console.log('What number is missing in the progression?');
 
-console.log('What number is missing in the progression?')
+function progress(mul, count, number) {
+  const sum = [];
+  let num = number;
+
+  for (let i = 1; i < count; i += 1) {
+    num += mul;
+    sum.push(num);
+  }
+  return sum;
+}
 
 let i = 0;
 do {
   const rand = Math.floor(Math.random() * (100));
-  const rand2 = Math.floor(Math.random() * (100)/5);
+  const rand2 = Math.floor((Math.random() * (100)) / 5);
 
   const randomOperator = Math.floor(Math.random() * (11 - 1) + 0);
 
   const all = progress(rand, 11, rand2);
 
-  let allDot = [];
+  const allDot = [];
   let example;
 
-  for (let i = 0; i < 10; i += 1) {
-    if (i !== randomOperator) {
-      allDot.push(all[i]);
-    }
-    else {
-      example = all[i];
+  for (let j = 0; j < 10; j += 1) {
+    if (j !== randomOperator) {
+      allDot.push(all[j]);
+    } else {
+      example = all[j];
       allDot.push('..');
     }
   }
@@ -36,25 +44,13 @@ do {
 
   if (example === answer && i < 2) {
     console.log('Correct!');
-  } 
-  else if (i === 2 && example === answer) {
+  } else if (i === 2 && example === answer) {
     console.log('Correct!');
     console.log(`Congratulations, ${name}!`);
-  }
-  else {
-    console.log( `'${answer}' is wrong answer ;(. Correct answer was '${example}'.`);
+  } else {
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${example}'.`);
     console.log(`Let's try again, ${name}!`);
     i += 3;
   }
   i += 1;
 } while (i < 3);
-
-function progress(mul, count, number) {
-  let sum = [];
-
-  for(let i = 1; i < count; i++) {
-    number += mul;
-    sum.push(number);
-  }
-  return sum;
-}
