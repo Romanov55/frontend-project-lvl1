f30c3a615ba3637bb02454a9a666063c967013a2
Romanov55 <Romanov20let@gmail.com>
2022-10-19 14:07:37 +0600

    add brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import readlineSync from 'readline-sync';
-import { name } from '../src/index.js';
+import { name, comparison } from '../src/index.js';
 
 console.log("Answer 'yes' if number even otherwise answer 'no'");
 
@@ -20,16 +20,12 @@ do {
     example = 'no';
   }
 
-  if (example === answer && i < 2) {
-    console.log('Correct!');
-  } else if (i === 2 && example === answer) {
-    console.log('Correct!');
-    console.log(`Congratulations, ${name}!`);
-  } else if (answer === 'yes') {
+  comparison(example, answer, i);
+  if (answer === 'yes' && example !== answer) {
     console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
     console.log(`Let's try again, ${name}!`);
     i += 3;
-  } else {
+  } else if (answer === 'no' && example !== answer) {
     console.log("'no' is wrong answer ;(. Correct answer was 'yes'.");
     console.log(`Let's try again, ${name}!`);
     i += 3;
