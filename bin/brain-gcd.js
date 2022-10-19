bd407bf5271ed563d2d4bb975387cc27c44f0ffd
Romanov55 <Romanov20let@gmail.com>
2022-10-19 16:44:37 +0600

    add brain-gcd

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,29 +1,27 @@
 #!/usr/bin/env node
 
 import readlineSync from 'readline-sync';
-import { name, divisionWithoutRemainder, comparison } from '../src/index.js';
+import { name, comparison } from '../src/index.js';
 
 console.log('Find the greatest common divisor of given numbers.');
 
 let i = 0;
 do {
-  let randomNumber1 = Math.floor(Math.random() * (100)+1);
+  let randomNumber1 = Math.floor(Math.random() * (100) + 1);
   let randomNumber2 = Math.floor((Math.random() * (100)) / 5 + 2);
 
   console.log(`Question: ${randomNumber1} ${randomNumber2}`);
 
   const answer = Number(readlineSync.question('Your answer: '));
 
-  let example;
-  while (randomNumber1 != randomNumber2) {
-  if (randomNumber1 > randomNumber2) {
-    randomNumber1 = randomNumber1 -randomNumber2;
+  while (randomNumber1 !== randomNumber2) {
+    if (randomNumber1 > randomNumber2) {
+      randomNumber1 -= randomNumber2;
+    } else {
+      randomNumber2 -= randomNumber1;
+    }
   }
-  else {
-    randomNumber2 = randomNumber2 - randomNumber1;
-  }
-}
-example = randomNumber1;
+  const example = randomNumber1;
 
   comparison(example, answer, i);
 
