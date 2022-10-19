0241c7d976e690b375fff10fc3e64f9fb94cf83a
Romanov55 <Romanov20let@gmail.com>
2022-10-19 16:39:55 +0600

    add brain-gcd

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -7,44 +7,23 @@ console.log('Find the greatest common divisor of given numbers.');
 
 let i = 0;
 do {
-  const randomNumber1 = Math.floor(Math.random() * (100)+1);
-  const randomNumber2 = Math.floor((Math.random() * (100)) / 5 + 2);
+  let randomNumber1 = Math.floor(Math.random() * (100)+1);
+  let randomNumber2 = Math.floor((Math.random() * (100)) / 5 + 2);
 
   console.log(`Question: ${randomNumber1} ${randomNumber2}`);
 
   const answer = Number(readlineSync.question('Your answer: '));
-  
-  const ran1 = randomNumber1 + 2;
-  const ran2 = randomNumber2 + 2;
-  const massiv1 = [];
-  const massiv2 = [];
-
-  for (let j = 0; j < ran1; j += 1) {
-    if (randomNumber1 % j === 0) {
-      massiv1.push(j);
-    }
-  }
-
-  for (let j = 0; j < ran2; j += 1) {
-    if (randomNumber2 % j === 0) {
-      massiv2.push(j);
-    }
-  }
-
-  let moreResult;
-  if (massiv1.length > massiv2.length) {
-    moreResult = massiv1.length;
-  } else {
-    moreResult = massiv2.length;
-  }
 
   let example;
-
-  for (let q = 0; q < moreResult; q += 1) {
-    if (massiv1[q] === massiv2[q]) {
-      example = massiv1[q];
-    }
+  while (randomNumber1 != randomNumber2) {
+  if (randomNumber1 > randomNumber2) {
+    randomNumber1 = randomNumber1 -randomNumber2;
   }
+  else {
+    randomNumber2 = randomNumber2 - randomNumber1;
+  }
+}
+example = randomNumber1;
 
   comparison(example, answer, i);
 
