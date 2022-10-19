ee14acdab4b50f167a784627af67d0b5146b25f5
Romanov55 <Romanov20let@gmail.com>
2022-10-19 14:08:39 +0600

    add brain-prime

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import readlineSync from 'readline-sync';
-import { name, divisionWithoutRemainder } from '../src/index.js';
+import { name, divisionWithoutRemainder, comparison } from '../src/index.js';
 
 console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
 
@@ -13,7 +13,7 @@ do {
 
   const massiv = [];
 
-  const noRemainder = divisionWithoutRemainder(randonNumber,massiv);
+  const noRemainder = divisionWithoutRemainder(randonNumber, massiv);
 
   let example;
 
@@ -25,16 +25,12 @@ do {
 
   const answer = readlineSync.question('Your answer: ');
 
-  if (example === answer && i < 2) {
-    console.log('Correct!');
-  } else if (i === 2 && example === answer) {
-    console.log('Correct!');
-    console.log(`Congratulations, ${name}!`);
-  } else if (answer === 'yes') {
+  comparison(example, answer, i);
+  if (answer === 'yes' && example !== answer) {
     console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
     console.log(`Let's try again, ${name}!`);
     i += 3;
-  } else {
+  } else if (answer === 'no' && example !== answer) {
     console.log("'no' is wrong answer ;(. Correct answer was 'yes'.");
     console.log(`Let's try again, ${name}!`);
     i += 3;
