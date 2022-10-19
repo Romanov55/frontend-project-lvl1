1c5c7bb81bf2283d937465024c8de0272c7dd0eb
Romanov55 <Romanov20let@gmail.com>
2022-10-19 14:06:48 +0600

    add index

--- src/index.js
+++ src/index.js
@@ -7,19 +7,19 @@ export const name = readlineSync.question('May I have your name? ');
 export const hello = console.log(`Hello, ${name}!`);
 
 export const divisionWithoutRemainder = (number, result) => {
-    for (let j = 0; j < number; j += 1) {
-        if (number % j === 0) {
-        result.push(j);
-        }
+  for (let j = 0; j < number; j += 1) {
+    if (number % j === 0) {
+      result.push(j);
     }
-    return result;
-}
+  }
+  return result;
+};
 
 export const comparison = (number1, number2, item) => {
-    if (number1 === number2 && item < 2) {
-        console.log('Correct!');
-    } else if (item === 2 && number1 === number2) {
-        console.log('Correct!');
-        console.log(`Congratulations, ${name}!`);
-    }
-}
+  if (number1 === number2 && item < 2) {
+    console.log('Correct!');
+  } else if (item === 2 && number1 === number2) {
+    console.log('Correct!');
+    console.log(`Congratulations, ${name}!`);
+  }
+};
