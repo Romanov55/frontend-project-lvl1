963fbd044dff171c8f9b8d67358d2aad084ad112
Romanov55 <Romanov20let@gmail.com>
2022-10-19 13:23:54 +0600

    add index

--- src/index.js
+++ src/index.js
@@ -5,3 +5,12 @@ export const welcome = console.log('Welcome to the Brain Games!');
 export const name = readlineSync.question('May I have your name? ');
 
 export const hello = console.log(`Hello, ${name}!`);
+
+export const divisionWithoutRemainder = (number, result) => {
+    for (let j = 0; j < number; j += 1) {
+        if (number % j === 0) {
+        result.push(j);
+        }
+    }
+    return result;
+}
