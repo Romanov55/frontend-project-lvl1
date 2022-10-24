b3af62567ef7a7255d8a3e602b0dc8b26d9d0d9b
Romanov55 <Romanov20let@gmail.com>
2022-10-25 00:20:02 +0600

    add even

---
+++ src/games/even.js
@@ -0,0 +1,28 @@
+import readlineSync from 'readline-sync';
+
+export const solutionEven = (name, comparisonYesNo) => {
+    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+    let i = 0;
+    do {
+    const randonNumber = Math.floor(Math.random() * (100));
+
+    console.log(`Question: ${randonNumber}`);
+
+    const answer = readlineSync.question('Your answer: ');
+
+    let example;
+    if (randonNumber % 2 === 0) {
+        example = 'yes';
+    } else {
+        example = 'no';
+    }
+
+    comparisonYesNo(example, answer, i, name);
+    if (example !== answer) {
+        i += 3;
+    }
+    i += 1;
+    } while (i < 3);
+
+}
