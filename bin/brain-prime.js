31fa580c6aba9f3c7e2d7f3c01523868bdc19a8f
Romanov55 <Romanov20let@gmail.com>
2022-10-25 00:18:36 +0600

    add brain-prime

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,43 +1,6 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
 import { name, comparisonYesNo } from '../src/index.js';
+import { solutionPrime } from '../src/games/prime.js';
 
-console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-
-function isPrime(n) {
-  if (n < 2) {
-    return 'no';
-  }
-  if (n === 2) {
-    return 'yes';
-  }
-
-  let j = 2;
-  const limit = Math.sqrt(n);
-  while (j <= limit) {
-    if (n % j === 0) {
-      return 'no';
-    }
-    j += 1;
-  }
-
-  return 'yes';
-}
-
-let i = 0;
-do {
-  const randonNumber = Math.floor(Math.random() * (100));
-
-  console.log(`Question: ${randonNumber}`);
-
-  const example = isPrime(randonNumber);
-
-  const answer = readlineSync.question('Your answer: ');
-
-  comparisonYesNo(example, answer, i, name);
-  if (answer !== 'no' && example !== answer) {
-    i += 3;
-  }
-  i += 1;
-} while (i < 3);
+solutionPrime(name, comparisonYesNo);
