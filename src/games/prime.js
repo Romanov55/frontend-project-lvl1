3ab44d59e1e1c2f7312170487d846f1e32b6618f
Romanov55 <Romanov20let@gmail.com>
2022-10-25 00:20:34 +0600

    add prime

---
+++ src/games/prime.js
@@ -0,0 +1,42 @@
+import readlineSync from 'readline-sync';
+
+export const solutionPrime = (name, comparisonYesNo) => {
+    console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+
+    function isPrime(n) {
+    if (n < 2) {
+        return 'no';
+    }
+    if (n === 2) {
+        return 'yes';
+    }
+
+    let j = 2;
+    const limit = Math.sqrt(n);
+    while (j <= limit) {
+        if (n % j === 0) {
+        return 'no';
+        }
+        j += 1;
+    }
+
+    return 'yes';
+    }
+
+    let i = 0;
+    do {
+    const randonNumber = Math.floor(Math.random() * (100));
+
+    console.log(`Question: ${randonNumber}`);
+
+    const example = isPrime(randonNumber);
+
+    const answer = readlineSync.question('Your answer: ');
+
+    comparisonYesNo(example, answer, i, name);
+    if (answer !== 'no' && example !== answer) {
+        i += 3;
+    }
+    i += 1;
+    } while (i < 3);
+}
