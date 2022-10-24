c75f77ccd03f200d95099d00d7db385b44a9d3d5
Romanov55 <Romanov20let@gmail.com>
2022-10-25 00:19:47 +0600

    add calc

---
+++ src/games/calc.js
@@ -0,0 +1,35 @@
+import readlineSync from 'readline-sync';
+
+export const solutionCalc = (name, comparison) => {
+    console.log('What is the result of the expression?');
+
+    let i = 0;
+    do {
+    const randomNumber1 = Math.floor(Math.random() * (100));
+    const randomNumber2 = Math.floor(Math.random() * (100));
+
+    const randomOperator = Math.floor(Math.random() * (3 - 0) + 0);
+
+    let example;
+
+    if (randomOperator === 0) {
+        console.log(`Question: ${randomNumber1} + ${randomNumber2}`);
+        example = randomNumber1 + randomNumber2;
+    } else if (randomOperator === 1) {
+        console.log(`Question: ${randomNumber1} - ${randomNumber2}`);
+        example = randomNumber1 - randomNumber2;
+    } else {
+        console.log(`Question: ${randomNumber1} * ${randomNumber2}`);
+        example = randomNumber1 * randomNumber2;
+    }
+
+    const answer = Number(readlineSync.question('Your answer: '));
+
+    comparison(example, answer, i, name);
+    if (example !== answer) {
+        i += 3;
+    }
+    i += 1;
+    } while (i < 3);
+
+}
