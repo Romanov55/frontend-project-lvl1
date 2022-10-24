21e04f01ff2aa0852ab7f450fd78af97ad6f0fd8
Romanov55 <Romanov20let@gmail.com>
2022-10-25 00:17:50 +0600

    add brain-evem

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,28 +1,6 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
 import { name, comparisonYesNo } from '../src/index.js';
+import { solutionEven } from '../src/games/even.js'
 
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-let i = 0;
-do {
-  const randonNumber = Math.floor(Math.random() * (100));
-
-  console.log(`Question: ${randonNumber}`);
-
-  const answer = readlineSync.question('Your answer: ');
-
-  let example;
-  if (randonNumber % 2 === 0) {
-    example = 'yes';
-  } else {
-    example = 'no';
-  }
-
-  comparisonYesNo(example, answer, i, name);
-  if (example !== answer) {
-    i += 3;
-  }
-  i += 1;
-} while (i < 3);
+solutionEven(name, comparisonYesNo);
