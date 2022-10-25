5bad9cadb126fddf27f9981054f8cec07af3400c
Romanov55 <Romanov20let@gmail.com>
2022-10-25 12:37:27 +0600

    add brain-calc

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
 import { name, comparison } from '../src/index.js';
-import { solutionCalc } from '../src/games/calc.js';
+import solutionCalc from '../src/games/calc.js';
 
 solutionCalc(name, comparison);
