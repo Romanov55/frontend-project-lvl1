1b0bbc7b2657c133a354dd517c7a5b2a322fb1f2
Romanov55 <Romanov20let@gmail.com>
2022-10-25 18:08:36 +0600

    add brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-import { name, comparisonYesNo } from '../src/index.js';
-import { solutionEven, exercise } from '../src/games/even.js';
+import solutionEven from '../src/games/even.js';
 
-solutionEven(name, comparisonYesNo);
+solutionEven();
