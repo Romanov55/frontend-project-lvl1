7bba5a30d10fe7018b27270d49f3dba3a6613f3d
Romanov55 <Romanov20let@gmail.com>
2022-10-25 12:40:45 +0600

    add progression

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,21 +1,21 @@
 import readlineSync from 'readline-sync';
 
-export const solutionProgression = (name) => {
-    console.log('What number is missing in the progression?');
+const solutionProgression = (name) => {
+  console.log('What number is missing in the progression?');
 
-    const progress = (mul, count, number) => {
+  const progress = (mul, count, number) => {
     const sum = [];
     let num = number;
 
     for (let i = 1; i < count; i += 1) {
-        num += mul;
-        sum.push(num);
+      num += mul;
+      sum.push(num);
     }
     return sum;
-    };
+  };
 
-    let i = 0;
-    do {
+  let i = 0;
+  do {
     const randomNumber1 = Math.floor(Math.random() * (100));
     const randomNumber2 = Math.floor((Math.random() * (100)) / 5);
     const randomTen = Math.floor(Math.random() * (11 - 1) + 0);
@@ -27,12 +27,12 @@ export const solutionProgression = (name) => {
     let example;
 
     for (let j = 0; j < 10; j += 1) {
-        if (j !== randomTen) {
+      if (j !== randomTen) {
         massivQuestion.push(allNumbers[j]);
-        } else {
+      } else {
         example = allNumbers[j];
         massivQuestion.push('..');
-        }
+      }
     }
     const finalQuestion = massivQuestion.join(' ');
 
@@ -41,15 +41,17 @@ export const solutionProgression = (name) => {
     const answer = Number(readlineSync.question('Your answer: '));
 
     if (example === answer && i < 2) {
-        console.log('Correct!');
+      console.log('Correct!');
     } else if (i === 2 && example === answer) {
-        console.log('Correct!');
-        console.log(`Congratulations, ${name}!`);
+      console.log('Correct!');
+      console.log(`Congratulations, ${name}!`);
     } else {
-        console.log(`'${answer}' is wrong answer ;(. Correct answer was '${example}'.`);
-        console.log(`Let's try again, ${name}!`);
-        i += 3;
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${example}'.`);
+      console.log(`Let's try again, ${name}!`);
+      i += 3;
     }
     i += 1;
-    } while (i < 3);
-}
+  } while (i < 3);
+};
+
+export default solutionProgression;
