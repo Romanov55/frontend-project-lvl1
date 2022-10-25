ebce5215b1278ec88471b8bf9dbacb4e59596cd3
Romanov55 <Romanov20let@gmail.com>
2022-10-25 18:57:58 +0600

    add brain-prime

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-import { name, comparisonYesNo } from '../src/index.js';
 import solutionPrime from '../src/games/prime.js';
 
-solutionPrime(name, comparisonYesNo);
+solutionPrime();
