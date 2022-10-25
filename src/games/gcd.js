ec747c6d004ff6e509c3b12bff37d708700a13b8
Romanov55 <Romanov20let@gmail.com>
2022-10-25 12:40:11 +0600

    add gcd

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,10 +1,10 @@
 import readlineSync from 'readline-sync';
 
-export const solutionGcd = (name, comparison) => {
-    console.log('Find the greatest common divisor of given numbers.');
+const solutionGcd = (name, comparison) => {
+  console.log('Find the greatest common divisor of given numbers.');
 
-    let i = 0;
-    do {
+  let i = 0;
+  do {
     let randomNumber1 = Math.floor(Math.random() * (100) + 1);
     let randomNumber2 = Math.floor((Math.random() * (100)) / 5 + 2);
 
@@ -13,20 +13,21 @@ export const solutionGcd = (name, comparison) => {
     const answer = Number(readlineSync.question('Your answer: '));
 
     while (randomNumber1 !== randomNumber2) {
-        if (randomNumber1 > randomNumber2) {
+      if (randomNumber1 > randomNumber2) {
         randomNumber1 -= randomNumber2;
-        } else {
+      } else {
         randomNumber2 -= randomNumber1;
-        }
+      }
     }
     const example = randomNumber1;
 
     comparison(example, answer, i, name);
 
     if (example !== answer) {
-        i += 3;
+      i += 3;
     }
     i += 1;
-    } while (i < 3);
+  } while (i < 3);
+};
 
-}
+export default solutionGcd;
