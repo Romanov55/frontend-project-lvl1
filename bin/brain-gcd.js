88c1f1087d90f149f0ea79bbd73ec425a60dc0e4
Romanov55 <Romanov20let@gmail.com>
2022-10-25 12:38:16 +0600

    add brain-gcd

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
 import { name, comparison } from '../src/index.js';
-import { solutionGcd } from '../src/games/gcd.js';
+import solutionGcd from '../src/games/gcd.js';
 
 solutionGcd(name, comparison);
