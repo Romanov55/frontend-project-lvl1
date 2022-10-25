7f28b71a020daf801f9860b15fb25ad8cb22d823
Romanov55 <Romanov20let@gmail.com>
2022-10-25 12:38:58 +0600

    add brain-progression

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
 import { name } from '../src/index.js';
-import { solutionProgression } from '../src/games/progression.js';
+import solutionProgression from '../src/games/progression.js';
 
 solutionProgression(name);
