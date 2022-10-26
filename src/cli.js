c8f284c9c5595dfafa547df72722b375035f2058
Romanov55 <Romanov20let@gmail.com>
2022-10-26 17:10:17 +0600

    up cli

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
 import readlineSync from 'readline-sync';
 
-const runBrainGames = () => {
-    console.log('Welcome to the Brain Games!');
-    const name = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${name}!`);
-}
+const startBrainGames = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+};
 
-export default runBrainGames;
+export default startBrainGames;
