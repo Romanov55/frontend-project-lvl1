485cf8af3543d31faab0c889d5f66e163afea80b
Romanov55 <Romanov20let@gmail.com>
2022-10-26 17:10:54 +0600

    up calc

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,12 +1,15 @@
-import gameEngine from '../index.js';
+import runGameEngine from '../index.js';
 import randomNumber from '../rand.js';
 
 const gameTask = 'What is the result of the expression?';
 
+const min1 = 1;
+
+const max1 = 3;
+const max2 = 100;
+
 const randomOperator = () => {
-  const min2 = 1;
-  const max2 = 3;
-  const randomOper = randomNumber(min2, max2);
+  const randomOper = randomNumber(min1, max1);
 
   if (randomOper === 0) {
     return '+';
@@ -16,14 +19,7 @@ const randomOperator = () => {
   return '*';
 };
 
-const data = () => {
-  const min1 = 1;
-  const max1 = 50;
-  const number1 = randomNumber(min1, max1);
-  const number2 = randomNumber(min1, max1);
-
-  const operator = randomOperator();
-
+const calculateQuestionAndAnswer = (number1, number2, operator) => {
   let question;
   let rightAnswer;
   if (operator === '+') {
@@ -36,10 +32,23 @@ const data = () => {
     question = `${number1} * ${number2}`;
     rightAnswer = number1 * number2;
   }
-  const result = [(question), String(rightAnswer)];
+  const result = [question, rightAnswer];
   return result;
 };
 
-const solutionCalc = () => gameEngine(data, gameTask);
+const calculateData = () => {
+  const number1 = randomNumber(min1, max2);
+  const number2 = randomNumber(min1, max2);
 
-export default solutionCalc;
+  const operator = randomOperator();
+
+  const [question, rightAnswer] = calculateQuestionAndAnswer(number1, number2, operator);
+
+  const result = [question, String(rightAnswer)];
+
+  return result;
+};
+
+const startCalc = () => runGameEngine(calculateData, gameTask);
+
+export default startCalc;
