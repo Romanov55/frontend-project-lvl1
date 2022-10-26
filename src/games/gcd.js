8b57618c39c1e7b5c2807904535cccdd2fd99868
Romanov55 <Romanov20let@gmail.com>
2022-10-26 17:29:41 +0600

    up gcd

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -11,7 +11,7 @@ const max2 = 100;
 const getCommonDivisor = (number1, number2) => {
   if (number2 > number1) return commonDivisor(number2, number1);
   if (!number2) return number1;
-  return commonDivisor(number2, number1 % number2);
+  return getCommonDivisor(number2, number1 % number2);
 };
 
 const calculateData = () => {
