34277048755251866a3b43a84dc547c0a5f229cf
Romanov55 <Romanov20let@gmail.com>
2022-10-27 15:10:24 +0600

    up prime

--- src/games/prime.js
+++ src/games/prime.js
@@ -11,12 +11,11 @@ const isPrime = (n) => {
     return false;
   }
 
-  for (let i = 3; i <= Math.sqrt(n); i += 1) {
+  for (let i = 2; i <= Math.sqrt(n); i += 1) {
     if (n % i === 0) {
       return false;
     }
   }
-
   return true;
 };
 
